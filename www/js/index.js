da1f1c0fc67d2fa097332dc64cb38cc2f9f10c87
mobilesupport <support@projeksistematik.com.my>
2016-09-22 15:55:07 +0800

    upd

--- www/js/index.js
+++ www/js/index.js
@@ -60,8 +60,14 @@ var app = {
         alert("Token ID = " + tokenID);
         //Token ID use for call notification form FCM server.
      }, function(error){
-        alert(error);
+        //alert(error);
      });
+         
+         navigator.FCMNotification.fcmReadAllMessages(function(result){
+             /* Return all message in json formate with id and message */
+           alert(result);
+         },function(e){alert(e)}
+         );
 
 
     },
