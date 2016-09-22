233be5108f5de76983eee080c63e31bd52d9db01
mobilesupport <support@projeksistematik.com.my>
2016-09-22 13:50:41 +0800

    upd

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,37 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
+        "id": "cordova-plugin-x-socialsharing.SocialSharing",
+        "pluginId": "cordova-plugin-x-socialsharing",
+        "clobbers": [
+            "window.plugins.socialsharing"
+        ]
+    },
+    {
+        "file": "plugins/Cordova-Plugin-FCMNotification/www/FCMNotificationPlugin.js",
+        "id": "Cordova-Plugin-FCMNotification.CDVPushyMe",
+        "pluginId": "Cordova-Plugin-FCMNotification",
+        "clobbers": [
+            "navigator.FCMNotification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-fcm/www/FCMPlugin.js",
+        "id": "cordova-plugin-fcm.FCMPlugin",
+        "pluginId": "cordova-plugin-fcm",
+        "clobbers": [
+            "FCMPlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-x-socialsharing": "5.1.3",
+    "Cordova-Plugin-FCMNotification": "1.0.0-dev",
+    "cordova-plugin-fcm": "1.1.4"
+}
+// BOTTOM OF METADATA
+});
