62d2239dc9d8a783c9238177c99ab200a9bf99f6
mobilesupport <support@projeksistematik.com.my>
2016-09-22 16:05:02 +0800

    upd

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -53,29 +53,21 @@ var app = {
     
      initPushNotificationRegister: function(){
                   
-          FCMPlugin.onNotification(
-          function(data){
-            setTimeout(function(){
-                alert(data.notification.body);
-            }, 100);
-            alert('data');
-          },
-          function(data){
-            alert('succsess');
-          },
-          function(data){
-            alert('error');
-          }
-        );
 
-//           navigator.FCMNotification.fcmTokenID(function(tokenID){
-//               
-//        // retrun token id for notification service
-//        alert("Token ID = " + tokenID);
-//        //Token ID use for call notification form FCM server.
-//     }, function(error){
-//        alert(error);
-//     });
+           navigator.FCMNotification.fcmTokenID(function(tokenID){
+               
+        // retrun token id for notification service
+        alert("Token ID = " + tokenID);
+        //Token ID use for call notification form FCM server.
+     }, function(error){
+        //alert(error);
+     });
+         
+         navigator.FCMNotification.fcmReadAllMessages(function(result){
+             /* Return all message in json formate with id and message */
+           alert(result);
+         },function(e){alert(e)}
+         );
 
 
     },
