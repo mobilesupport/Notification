6ac1d3f9c1216a6a9f3e9f9e57d66eb2fc90a6f9
mobilesupport <support@projeksistematik.com.my>
2016-09-22 10:35:48 +0800

    udp

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -38,6 +38,9 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+        
+        app.initPushNotificationRegister();
+        
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -46,5 +49,109 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+    
+     initPushNotificationRegister: function(){
+    
+           navigator.FCMNotification.fcmTokenID(function(tokenID){
+               
+               alert("uuu");
+        // retrun token id for notification service
+        alert("Token ID = " + tokenID);
+        //Token ID use for call notification form FCM server.
+     }, function(error){
+        alert(error);
+     });
+//        var pushNotification = window.plugins.pushNotification;
+//        
+//        
+//        if ( device.platform == 'android' || device.platform == 'Android'){
+//            pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"227139393233","ecb":"app.onNotificationGCM"});
+//        } 
+//        else {
+//            pushNotification.register(app.tokenHandler,app.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"});
+//        }
+
+    },
+    
+    // result contains any message sent from the plugin call
+    successHandler: function(result) {
+//        alert('Callback Success! Result = '+result);
+    },
+    
+    errorHandler:function(error) {
+//        alert(error);
+    },
+    
+    onNotificationGCM: function(e) {
+        switch( e.event )
+        {
+            case 'registered':
+//                $("#redidtxtareas").val(e.regid);
+                if ( e.regid.length > 0 )
+                {
+//                    console.log("Regid " + e.regid);
+//                    alert('registration id = '+e.regid);
+                    dbmanager.getProfile(function(returnData){
+                        if(returnData.rows.length>0){
+                            var uid=returnData.rows.item(0).uid;
+                            var token=returnData.rows.item(0).token;
+                            var regid=e.regid;
+                            
+                            postRegistrationId(uid, token, regid, "android"); 
+                        }    
+                    });
+                }
+            break;
+ 
+            case 'message':
+              // this is the actual push notification. its format depends on the data model from the push server
+//              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+            break;
+ 
+            case 'error':
+//              alert('GCM error = '+e.msg);
+            break;
+ 
+            default:
+//              alert('An unknown GCM event has occurred');
+              break;
+        }
+    },
+    
+    tokenHandler: function(result) {
+        // Your iOS push server needs to know the token before it can push to this device
+        // here is where you might want to send it the token for later use.
+//        alert('device token = ' + result);
+//        $("#redidtxtareas").val(result);
+        dbmanager.getProfile(function(returnData){
+            if(returnData.rows.length>0){
+                var uid=returnData.rows.item(0).uid;
+                var token=returnData.rows.item(0).token;
+                var regid=result;
+                            
+                postRegistrationId(uid, token, regid, "ios"); 
+            }    
+        });
+    },
+    
+    onNotificationAPN: function(event) {
+        if ( event.alert )
+        {
+            navigator.notification.alert(event.alert);
+        }
+
+        if ( event.sound )
+        {
+            var snd = new Media(event.sound);
+            snd.play();
+        }
+
+        if ( event.badge )
+        {
+            pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, event.badge);
+        }
     }
+    
+    
 };
